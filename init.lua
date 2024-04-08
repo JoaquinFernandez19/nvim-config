d1afed79dd5b20b28b68bf75f2cb62dfd3ef56cf
jfernandez <jfernandez@brinta.com>
2024-04-08 00:00:11 -0300

    nvim config first commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+--require("cattpucin").setup({
+--  transparent_background = false,
+--})
+
+vim.cmd.colorscheme("kanagawa")
+
+--require("notify").setup({
+--  background_colour = "#000000",
+--})
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+{ "catppuccin/nvim", name = "catppuccin"  },
+{ "morhetz/gruvbox", name = "gruvbox"  },
+{ "rebelot/kanagawa.nvim", name = "kanagawa"  },
+{
+"LazyVim/LazyVim",
+  opts = {
+    colorscheme = "kanagawa",
+    },
+  },
+}
